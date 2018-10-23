1c788fe8e42fb0cd0c0c735d591ccb35fb107b36
Jason Beh <behjieshen@gmail.com>
2018-10-23 12:44:56 +0800

    desktop front end is done

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,31 +1,20 @@
 $(document).ready(function() {
-  // change formdata to json format
-  function getFormData($form) {
-    var unindexed_array = $form.serializeArray();
-    var indexed_array = {};
+  modalController();
+  optionButtonController();
 
-    $.map(unindexed_array, function(n, i) {
-      indexed_array[n['name']] = n['value'];
-    });
-
-    return indexed_array;
-  }
-
+  // when form is submit
   $('form').submit(function(e) {
     e.preventDefault();
     var data = getFormData($(this));
     var bmr;
     if (data.gender == 'male') {
-      console.log('male');
       bmr = 66 + 13.75 * data.weight + 5 * data.height - 6.8 * data.age;
     } else {
-      console.log('female');
       bmr = 655 + 9.6 * data.weight + 1.8 * data.height - 4.7 * data.age;
     }
     switch (data.activity) {
       case 'lightly-active':
         bmr *= 1.1;
-        console.log('lightly');
         break;
       case 'moderately-active':
         bmr *= 1.275;
@@ -43,64 +32,91 @@ $(document).ready(function() {
     switch (data.goal) {
       case 'build-muscle':
         bmr += 200;
-        console.log('muscle');
         break;
       case 'lose-fat':
         bmr -= 200;
-        console.log('lose-fat');
         break;
       case 'maintainence':
         bmr -= 100;
-        console.log('maintainence');
         break;
       default:
         break;
     }
-
     var finalBMR = Math.round(bmr);
-    console.log(bmr);
-    console.log(finalBMR);
-    $('.bmr-form').hide();
-    $('.bmr-results').css('display', 'block');
     $('.bmr-results-content')
       .children('h6')
       .text(`${finalBMR} calories`);
+    transitionToResults();
+
+    $('.bmr-results-content span').click(function() {
+      $('form')[0].reset();
+      $('.bmr-form').css('display', 'block');
+      $('.bmr-results').hide();
+    });
+  });
+
+  function optionButtonController() {
+    $('.bmr-option-button').click(function() {
+      // remove all option in same category
+      $(this)
+        .siblings('.bmr-option-button')
+        .removeClass('active');
+      // make current button to be active
+      $(this).addClass('active');
+      // get the text of button
+      var text = $(this)
+        .children('span')[0]
+        .textContent.toLowerCase()
+        .replace(' ', '-');
+      // handle the checkbox appropirately
+      $(this)
+        .siblings('select')
+        .children('option')
+        .removeAttr('selected');
+      $(`[value=${text}]`).attr('selected', 'true');
+    });
+  }
+
+  function transitionToResults() {
+    $('.bmr-form').hide();
+    $('.bmr-results').css('display', 'block');
 
     setTimeout(function() {
       $('.bmr-results-content').css('display', 'block');
       $('.spinner').hide();
     }, 2000);
-  });
+  }
 
-  $('.bmr-option-button').click(function() {
-    $(this)
-      .siblings('.bmr-option-button')
-      .removeClass('active');
-    $(this).addClass('active');
-    var text = $(this)
-      .children('span')[0]
-      .textContent.toLowerCase()
-      .replace(' ', '-');
-    $(this)
-      .siblings('select')
-      .children('option')
-      .removeAttr('selected');
-    console.log();
-    $(`[value=${text}]`).attr('selected', 'true');
-  });
+  // change formdata to json format
+  function getFormData($form) {
+    var unindexed_array = $form.serializeArray();
+    var indexed_array = {};
 
-  $('.bmr-modal-trigger').click(function() {
-    $('.bmr-modal').addClass('active');
-  });
+    $.map(unindexed_array, function(n, i) {
+      indexed_array[n['name']] = n['value'];
+    });
 
-  $('.bmr-modal-close').click(function() {
-    $('.bmr-modal').removeClass('active');
-  });
+    return indexed_array;
+  }
 
-  $('.bmr-modal').mouseup(function(e) {
-    var container = $('.bmr-modal');
-    if (container.is(e.target) && container.has(e.target).length === 0) {
+  // Close and open modal
+  function modalController() {
+    // open modal
+    $('.bmr-modal-trigger').click(function() {
+      $('.bmr-modal').addClass('active');
+    });
+
+    // close modal when click on X button
+    $('.bmr-modal-close').click(function() {
       $('.bmr-modal').removeClass('active');
-    }
-  });
+    });
+
+    // close modal when click on overlay
+    $('.bmr-modal').mouseup(function(e) {
+      var container = $('.bmr-modal');
+      if (container.is(e.target) && container.has(e.target).length === 0) {
+        $('.bmr-modal').removeClass('active');
+      }
+    });
+  }
 });
